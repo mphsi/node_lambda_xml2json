df9891785884ec5874a85fa9436c4cd1cca182db
Paul Hernández <mpaul.hernandez@gmail.com>
2019-06-19 11:37:16 -0500

    Fix result variable assignment

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var xml2js = require('./node_modules/xml2js');
 
 exports.handler = async (event) => {
     const xml  = event['body-json'];
-    const json = xml2js.parseString(
+    let   json = null;
+    
+    xml2js.parseString(
       xml,
       {
         mergeAttrs: true,
         explicitArray: false
       }, function (err, result) {
-        return JSON.stringify(result);
+        json = JSON.stringify(result);
     });
 
     const response = {
